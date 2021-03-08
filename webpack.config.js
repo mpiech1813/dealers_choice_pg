5db4fca5ffd86516e10411097345c42af638b0f4
Maciej Pie <maciejpiech18@gmail.com>
2021-03-08 15:09:42 -0600

    going to deploy

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
